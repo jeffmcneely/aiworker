085a1111572ec9cb2408c649e0b26115414a96e7
jeff mcneely <jeff@mcneely.io>
2025-08-14 16:40:32 -0500

    change _app

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,7 +1,37 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import '@aws-amplify/ui-react/styles.css'
-import '../lib/amplify-config'
+import { Amplify } from 'aws-amplify'
+
+// Configure Amplify
+const amplifyConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-west-2_KvNDBMljE',
+      userPoolClientId: '6sc0nqta7a2kt727shji0qqeen',
+      identityPoolId: 'us-west-2:c839acd7-81ed-4266-8776-89068288760c',
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: 'code' as const,
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+}
+
+Amplify.configure(amplifyConfig)
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
